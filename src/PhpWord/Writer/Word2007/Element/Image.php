54362ca7b5994bc76d4d69b28e3d11e1841eb01f
TarikHusovicContentoo <102807100+TarikHusovicContentoo@users.noreply.github.com>
2024-05-07 12:42:13 +0200

    Update Image.php

--- src/PhpWord/Writer/Word2007/Element/Image.php
+++ src/PhpWord/Writer/Word2007/Element/Image.php
@@ -57,7 +57,11 @@ class Image extends AbstractElement
         $rId = $element->getRelationId() + ($element->isInSection() ? 6 : 0);
         $style = $element->getStyle();
         $styleWriter = new ImageStyleWriter($xmlWriter, $style);
-
+        
+        $style->setUnit('pt');
+        $style->setWidth(number_format(\PhpOffice\PhpWord\Shared\Converter::pixelToPoint($style->getWidth()), 2, '.', ''));
+        $style->setHeight(number_format(\PhpOffice\PhpWord\Shared\Converter::pixelToPoint($style->getHeight()), 2, '.', ''));
+        
         if (!$this->withoutP) {
             $xmlWriter->startElement('w:p');
             $styleWriter->writeAlignment();
